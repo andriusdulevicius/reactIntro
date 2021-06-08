70d4ff21fdb5b5dc2cb25df8c1ceadf1d1d828a0
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-08 11:01:25 +0300

    images, conditional class , and counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  //lokali komponento busena
+  state = {
+    count: 0,
+    imgUrl: 'https://picsum.photos/200/200',
+  };
+
+  h2ElStyles = {
+    background: 'tomato',
+    color: 'snow',
+    textAlign: 'center',
+  };
+  //Javascriptas jsx veikia riestiniuose skliausteliuose render() metode {} , nes render kaip ir state yra spec reacto sukurti metodai
   render() {
     return (
       <div className='container mt-4'>
-        <h2> I am react Counter component</h2>
-        <span className='badge badge-info mr-3'>10</span>
+        <h2 style={{ fontSize: '2em' }}> I am react Counter component</h2>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className='btn btn-warning'> Press me </button>
+        <div className='mt-4'>
+          <img src={this.state.imgUrl} alt='image' />
+        </div>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let badgeClasses = 'badge mr-3 badge-';
+    badgeClasses += this.state.count > 0 ? 'info' : 'danger';
+    return badgeClasses;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count > 0 ? count : 'Out of stock';
   }
 }
 
