cc674a096869b8aea4f0e93d37467dce03bd1bde
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-10 11:38:17 +0300

    positive counters found

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
 class NavBar extends Component {
+  countPositiveValues() {
+    const filtered = this.props.counters.filter((c) => c.value > 0);
+    return filtered.length;
+  }
+
   render() {
     return (
       <nav className='navbar navbar-expand-lg navbar-light bg-light'>
         <span className='navbar-brand mb-0'>
-          <strong>Counters </strong> Anywhere | <strong>{this.props.counters.length}</strong>
+          <strong>Counters </strong> Anywhere | <strong> Total Counters {this.props.counters.length}</strong> | Positive
+          counters :{this.countPositiveValues()}
         </span>
-        {/* {this.props.counters.map((c) => (
-          <a href='/' key={c.id}>
-            {c.id}
-          </a>
-        ))} */}
       </nav>
     );
   }
